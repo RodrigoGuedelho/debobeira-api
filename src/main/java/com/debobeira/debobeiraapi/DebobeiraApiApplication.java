94552efd150644bd910044dda8e8062ed2060d19
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2023-12-20 22:35:29 -0300

    Initial commit.

---
+++ src/main/java/com/debobeira/debobeiraapi/DebobeiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.debobeira.debobeiraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebobeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebobeiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debobeira/debobeiraapi/DebobeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debobeira.debobeiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebobeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
